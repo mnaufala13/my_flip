866c351cc4a61c53e60119dfdc54727efac1a494
Muhammad Naufal Abiyyu <naufal@oval.id>
2021-06-30 15:56:29 +0700

    - remove db transaction cause not used - fix error parse time cause getting datetime "0000-00-00 00:00:00" from flip service - update unit test

--- biglip/repository/svc/svc.go
+++ biglip/repository/svc/svc.go
@@ -11,13 +11,14 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/volatiletech/null/v8"
-	"github.com/volatiletech/sqlboiler/v4/boil"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type BigFlipSvcRepository interface {
@@ -49,17 +50,15 @@ func buildRequest(method, url string, body io.Reader) (*http.Request, error) {
 
 // Disburse call the bigflip api and then log the response
 func (f *flipper) Disburse(ctx context.Context, withdrawalId string, payload domain.DisbursePayload) (*domain.FlipTransaction, error) {
-	tx, err := f.db.BeginTx(ctx, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "error begin trx at disburse")
-	}
 	flipTrx, err := f.callDisburse(payload)
 	if err != nil {
 		return nil, err
 	}
 	go func() {
-		_, err := f.log(ctx, tx, withdrawalId, *flipTrx)
-		log.Println(err)
+		_, err := f.log(ctx, withdrawalId, *flipTrx)
+		if err != nil {
+			log.Println(err)
+		}
 	}()
 	return flipTrx, nil
 }
@@ -89,26 +88,62 @@ func (f *flipper) Status(ctx context.Context, transactionId int) (*domain.FlipTr
 	return flipTrx, nil
 }
 
-func (f *flipper) log(ctx context.Context, exec boil.ContextExecutor, withdrawalId string, ft domain.FlipTransaction) (*models.BigflipLog, error) {
-	lg := models.BigflipLog{
+func normalizeDatetime(datetime string) (string, error) {
+	if datetime == "0000-00-00 00:00:00" {
+		return "0001-01-01T00:00:00Z", nil
+	}
+	tt := strings.Split(datetime, " ")
+	if len(tt) != 2 {
+		return "", errors.New("invalid format")
+	}
+	dt := fmt.Sprintf("%sT%s+07:00", tt[0], tt[1])
+	return dt, nil
+}
+
+func flipTransactionToModel(ft domain.FlipTransaction) (*models.BigflipLog, error) {
+	tn, err := normalizeDatetime(ft.TimeServed)
+	if err != nil {
+		return nil, err
+	}
+	t, err := time.Parse(time.RFC3339, tn)
+	if err != nil {
+		return nil, errors.Wrap(err, "error parse time serve")
+	}
+	ttn, err := normalizeDatetime(ft.Timestamp)
+	if err != nil {
+		return nil, err
+	}
+	trxTimestamp, err := time.Parse(time.RFC3339, ttn)
+	if err != nil {
+		return nil, errors.Wrap(err, "error parse transaction timestamp")
+	}
+	lg := &models.BigflipLog{
 		TransactionID:   ft.Id,
 		Amount:          ft.Amount,
 		Status:          ft.Status,
-		TRXTimestamp:    null.TimeFrom(ft.Timestamp),
+		TRXTimestamp:    null.TimeFrom(trxTimestamp),
 		BankCode:        ft.BankCode,
 		AccountNumber:   ft.AccountNumber,
 		BeneficiaryName: ft.BeneficiaryName,
 		Remark:          ft.Remark,
 		Receipt:         ft.Receipt,
-		TimeServed:      null.TimeFrom(ft.TimeServed),
+		TimeServed:      null.TimeFrom(t),
 		Fee:             ft.Fee,
-		WithdrawalID:    withdrawalId,
 	}
-	_, err := f.psqlFlip.Insert(ctx, exec, lg)
+	return lg, nil
+}
+
+func (f *flipper) log(ctx context.Context, withdrawalId string, ft domain.FlipTransaction) (*models.BigflipLog, error) {
+	lg, err := flipTransactionToModel(ft)
 	if err != nil {
 		return nil, err
 	}
-	return &lg, nil
+	lg.WithdrawalID = withdrawalId
+	_, err = f.psqlFlip.Insert(ctx, f.db, *lg)
+	if err != nil {
+		return nil, err
+	}
+	return lg, nil
 }
 
 func (f flipper) callDisburse(payload domain.DisbursePayload) (*domain.FlipTransaction, error) {
--- biglip/repository/svc/svc_test.go
+++ biglip/repository/svc/svc_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func Test_buildRequest(t *testing.T) {
@@ -42,4 +43,40 @@ func Test_flipper_Status(t *testing.T) {
 	flipTrx, err := flipper.Status(context.Background(), id)
 	assert.NoError(t, err)
 	assert.EqualValues(t, id, flipTrx.Id)
+}
+
+func Test_flipTransactionToModel(t *testing.T) {
+	ft := domain.FlipTransaction{
+		Id:              161877137,
+		Amount:          11,
+		Status:          "PENDING",
+		Timestamp:       "2021-06-30 15:16:44",
+		BankCode:        "bni",
+		AccountNumber:   "10102020",
+		BeneficiaryName: "PT FLIP",
+		Remark:          "eeee",
+		Receipt:         "",
+		TimeServed:      "0000-00-00 00:00:00",
+		Fee:             400,
+	}
+	tModel, err := flipTransactionToModel(ft)
+	assert.NoError(t, err)
+	assert.EqualValues(t, ft.Id, tModel.TransactionID)
+	assert.EqualValues(t, ft.Amount, tModel.Amount)
+	assert.EqualValues(t, ft.Status, tModel.Status)
+	assert.EqualValues(t, "2021-06-30T15:16:44+07:00", tModel.TRXTimestamp.Time.Format(time.RFC3339))
+	assert.EqualValues(t, ft.BankCode, tModel.BankCode)
+	assert.EqualValues(t, ft.AccountNumber, tModel.AccountNumber)
+	assert.EqualValues(t, ft.BeneficiaryName, tModel.BeneficiaryName)
+	assert.EqualValues(t, ft.Remark, tModel.Remark)
+	assert.EqualValues(t, ft.Receipt, tModel.Receipt)
+	assert.EqualValues(t, "0001-01-01T00:00:00Z", tModel.TimeServed.Time.Format(time.RFC3339))
+	assert.EqualValues(t, ft.Fee, tModel.Fee)
+}
+
+func Test_normalizeDatetime(t *testing.T) {
+	nullDt := "0000-00-00 00:00:00"
+	dt, err := normalizeDatetime(nullDt)
+	assert.NoError(t, err)
+	assert.EqualValues(t, "0001-01-01T00:00:00Z", dt)
 }
